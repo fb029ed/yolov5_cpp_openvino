28cf2ebf5e0e1c440417abc7376c524dab1e005e
DYF-AI <54470509+DYF-AI@users.noreply.github.com>
2021-04-11 16:13:16 +0800

    Update detector.h

--- demo/detector.h
+++ demo/detector.h
@@ -31,7 +31,7 @@ private:
     double sigmoid(double x);
     vector<int> get_anchors(int net_grid);
     bool parse_yolov5(const Blob::Ptr &blob,int net_grid,float cof_threshold,
-        vector<Rect>& o_rect,vector<float>& o_rect_cof);
+        vector<Rect>& o_rect,vector<float>& o_rect_cof, vector<int> &classId);
     Rect detet2origin(const Rect& dete_rect,float rate_to,int top,int left);
     //存储初始化获得的可执行网络
     ExecutableNetwork _network;
@@ -41,5 +41,14 @@ private:
     string _xml_path;                             //OpenVINO模型xml文件路径
     double _cof_threshold;                //置信度阈值,计算方法是框置信度乘以物品种类置信度
     double _nms_area_threshold;  //nms最小重叠面积阈值
+    string className[80] = { "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+		 "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+		 "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+		 "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+		 "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+		 "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+		 "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+		 "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+		 "hair drier", "toothbrush" };
 };
 #endif
